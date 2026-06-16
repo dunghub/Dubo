6afa1aa56c6aeaef7190614a012dfe4caf93200e
dunghub <phamthucdung04012012@gmail.com>
2026-06-17 00:19:13 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Đã đăng nhập thành công bot: ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === '!ping') {
+        message.reply('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
